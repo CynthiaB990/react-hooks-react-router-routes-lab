899aba6ef7e03b237eff465305acfd388a22eee2
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-04-30 13:48:28 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const itemStyle = {
+  display: "block"
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink exact to="/" style={itemStyle}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies" style={itemStyle}>Movies</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors" style={itemStyle}>Directors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors" style={itemStyle}>Actors</NavLink>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default NavBar;
